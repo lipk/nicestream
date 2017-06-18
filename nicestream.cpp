7fc725d248a8a4937d1fbd420bf14e8b098bf2ea
Lipka Boldizsár <lipkab@zoho.com>
2017-06-18 11:08:46 +0200

    Fix sep logic.

--- nicestream.cpp
+++ nicestream.cpp
@@ -241,15 +241,21 @@ sep &sep::operator=(sep&& other) {
 }
 
 std::istream &operator >>(std::istream &is, sep what) {
-    bool is_valid = false;
+    bool is_valid = what.nfa_p->match() == match_state::ACCEPT;
+    std::vector<uint8_t> buf;
     while (true) {
-        is_valid = is_valid || what.nfa_p->match() == match_state::ACCEPT;
-        uint8_t next = static_cast<uint8_t>(is.peek());
+        uint8_t next = static_cast<uint8_t>(is.get());
+        buf.push_back(next);
         what.nfa_p->next(next);
-        if (what.nfa_p->match() == match_state::REFUSE) {
+        if (what.nfa_p->match() == match_state::ACCEPT) {
+            is_valid = true;
+            buf.clear();
+        } else if (what.nfa_p->match() == match_state::REFUSE) {
             break;
         }
-        is.get();
+    }
+    for (size_t i = buf.size(); i>0; --i) {
+        is.putback(buf[i-1]);
     }
     if (!is_valid) {
         throw invalid_input();
